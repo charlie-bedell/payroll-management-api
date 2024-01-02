6e0acbb4ac44fdc5a65731cf6dbbfc83d6541dbc
Charles Bedell <charlesbedell5@gmail.com>
2024-01-02 17:17:39 -0500

    toplevel seed script

---
+++ seed.js
@@ -0,0 +1,7 @@
+import { seedCompanyDatabase } from './config/seed/00-seed-company.js';
+import { seedEmployeeDatabase } from './config/seed/00-seed-employees.js';
+import { main as generateData } from './config/data/generate-dummy-data.js';
+
+generateData(7, 2); // generateData(num_employees, num_companies);
+seedCompanyDatabase().then(() => seedEmployeeDatabase());
+
