a531b18a69ae95e4d6f1c7f86050819afd6d9028
Charles Bedell <charlesbedell5@gmail.com>
2024-01-03 18:55:54 -0500

    Revert "auth router and logging middleware"
    This reverts commit f69bf16b2491134fc78f6ebe46036f6704623901.

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 import 'dotenv/config.js';
-import express from 'express';
-import cors from 'cors';
-import morgan from 'morgan'; // logging middleware
-
 import './config/database.js';
 import companiesRouter from './routes/company.js';
-import authRouter from './routes/auth.js';
+
+
+import express from 'express';
+import cors from 'cors';
+
+// TODO import routes
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -13,10 +14,8 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors());
-app.use(morgan('tiny'));
 
 app.use('/companies', companiesRouter);
-app.use('/user', authRouter);
 
 
 
