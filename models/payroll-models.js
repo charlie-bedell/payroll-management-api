00212932e47fb47a75bf5a6ac185e5d1736ba2de
Charles Bedell <charlesbedell5@gmail.com>
2024-01-05 11:43:13 -0500

    update to models, new userRole model to control permissions

--- models/payroll-models.js
+++ models/payroll-models.js
@@ -7,13 +7,13 @@ const companySchema = mongoose.Schema({
 );
 
 const employeeSchema = mongoose.Schema({
-	firstName: { type: String },
-	lastName: { type: String },
+	firstName: { type: String, required: true },
+	lastName: { type: String, required: true },
 	country: { type: String },
 	salary: { type: Number },
 	department: { type: String },
 	position: { type: String },
-	bonus: { type: Number },
+	bonus: { type: Number, default: 0},
 	start_date: { type: Date },
 	end_date: { type: Date },
 	companyId: { type: mongoose.Schema.Types.ObjectId, ref: 'Company' }
@@ -22,12 +22,20 @@ const employeeSchema = mongoose.Schema({
 
 const userSchema = mongoose.Schema({
 	username: { type: String, unique: true, required: true },
-	password: { type: String, required: true }
+	password: { type: String, required: true },
+  employeeId: { type: mongoose.Schema.Types.ObjectId, ref: 'Employee', default: null},
+  companyId: {type: mongoose.Schema.Types.ObjectId, ref: 'Company', default: null},
+  userRoleId: {type: mongoose.Schema.Types.ObjectId, ref: 'UserRole', default: null}
 },
 	{ timestamps: true });
+
+const userRoleSchema = mongoose.Schema({
+  role: { type: String,
+          enum: ['Employee', 'Admin', 'Manager'] },
+});
 
 const Company = mongoose.model('Company', companySchema);
 const Employee = mongoose.model('Employee', employeeSchema);
 const User = mongoose.model('User', userSchema);
-
-export { Company, Employee, User }
+const UserRole = mongoose.model('UserRole', userRoleSchema);
+export { Company, Employee, User, UserRole }
